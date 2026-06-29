aa89f6c8e2987e7d475e066b896346743d461468
danm7681-svg <danm7681@gmail.com>
2026-06-29 13:09:15 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<golfsimz_v1.App>("#app");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+await builder.Build().RunAsync();
